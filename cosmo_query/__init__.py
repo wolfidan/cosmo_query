4ae65667fefa3fde0f44242f942a494a3577712a
Daniel Wolfensberger <daniel.wolfensberger@epfl.ch>
2017-06-21 11:55:00 +0200

    Added way to store data obtained from extract.py functions

--- cosmo_query/__init__.py
+++ cosmo_query/__init__.py
@@ -1,2 +1,2 @@
-from retrieve_model_data import SSH, Query, load_netcdf, save_netcdf
+from retrieve_model_data import SSH, Query, load_netcdf, save_netcdf, load_hdf5, save_hdf5
 from extract import coords_profile, extract
